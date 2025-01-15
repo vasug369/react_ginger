82431282dcb68108c7b6dfb6405179f3599d02d1
vasug@369 <vasupankajdbd@gmail.com>
2025-01-15 13:19:24 +0530

    folder structire

---
+++ route_proj/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <div className="nav_container">
+            <li>Home</li>
+            <li>Contact</li>
+            <li>About</li>
+            <li>contact</li>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
